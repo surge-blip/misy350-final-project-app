55192a142036cc2addb255e59be80f87d2a580f4
Surge <surajiv@udel.edu>
2026-04-03 15:07:39 -0400

    session state added

--- app.py
+++ app.py
@@ -1,2 +1,17 @@
 import streamlit as st
 st.title("Small Business Inventory Manager")
+
+# Session Setup
+
+if "logged_in" not in st.session_state:
+    st.session_state["logged_in"] = False
+
+if "user" not in st.session_state:
+    st.session_state["user"] = None
+
+if "role" not in st.session_state:
+    st.session_state["role"] = None
+
+if "page" not in st.session_state:
+    st.session_state["page"] = "login"
+
