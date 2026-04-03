eacdee68f03b34fc6bb98a31c89a5f31d0e66f53
Surge <surajiv@udel.edu>
2026-04-03 17:17:00 -0400

    added register page with nagivation between pages

--- app.py
+++ app.py
@@ -17,24 +17,14 @@ if "page" not in st.session_state:
 
 # page routing
 
-elif st.session_state["page"] == "register":
-    st.header("Register Page")
-
-elif st.session_state["page"] == "owner_dashboard":
-    st.header("Owner Dashboard")
-
-elif st.session_state["page"] == "employee_dashboard":
-    st.header("Employee Dashboard")
-
-## login page
-
 if st.session_state["page"] == "login":
+
     st.title("Small Business Inventory Manager")
     st.write("Welcome to the inventory system")
     st.header("Login Page")
 
-    EMAIL = st.text_input("Email")
-    PASSWORD = st.text_input("Password")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
 
     if st.button("Login"):
         st.write("Login clicked")
@@ -42,6 +32,36 @@ if st.session_state["page"] == "login":
     if st.button("Go to Register"):
         st.session_state["page"] = "register"
         st.rerun()
+
+
+elif st.session_state["page"] == "register":
+
+    st.title("Register Account")
+    st.write("Create a new account")
+
+    name = st.text_input("Full Name")
+    email = st.text_input("Email")
+    password = st.text_input("Password", type="password")
+
+    role = st.selectbox("Role", ["Owner", "Employee"])
+
+    if st.button("Register"):
+        st.write("Account created")
+
+    if st.button("Go to Login"):
+        st.session_state["page"] = "login"
+        st.rerun()
+
+
+elif st.session_state["page"] == "owner_dashboard":
+    st.header("Owner Dashboard")
+
+elif st.session_state["page"] == "employee_dashboard":
+    st.header("Employee Dashboard")
+
+
+
+
 
 
 
